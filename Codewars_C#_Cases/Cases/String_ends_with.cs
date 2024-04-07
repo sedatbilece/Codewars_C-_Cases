e5daa7da1f851b3fb3f43f815f364d37dd49f543
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2024-04-07 16:19:36 +0300

    String_ends_with

---
+++ Codewars_C#_Cases/Cases/String_ends_with.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace Codewars_C__Cases.Cases
+{
+    public class String_ends_with
+    {
+
+        public static bool Solution(string str, string ending)
+        {
+
+            if (str.Length < ending.Length)
+                return false;
+
+            var check = str.Substring(str.Length - ending.Length);
+
+            if (check != ending)
+                return false;
+
+            return true;
+        }
+    }
+}
+
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
--- Codewars_C#_Cases/Program.cs
+++ Codewars_C#_Cases/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using Codewars_C__Cases.Cases;
+
+var result =  String_ends_with.Solution("abc", "bc");
+
+
+var i = 0;
