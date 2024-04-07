2d0d654c3f01fb77672cf481755aa319c229e3c1
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2024-04-07 16:35:46 +0300

    CompoundArray

---
+++ Codewars_C#_Cases/Cases/CompoundArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_C__Cases.Cases
+{
+    public class CompoundArray
+    {
+
+        public static int[] Solution(int[] a, int[] b)
+        {
+
+            int[] result = new int[a.Length + b.Length];
+            int j = 0;
+            var length = a.Length > b.Length ? a.Length : b.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if(i < a.Length)
+                {
+                    result[j] = a[i];
+                    j++;
+                }
+                if(i < b.Length)
+                {
+                    result[j] = b[i];
+                    j++;
+                }
+            }
+            return result;
+        }
+
+    }
+}
+/*
+You have to create a method "compoundArray" which should take as input two int arrays of different length and return one int array with numbers of both arrays shuffled one by one.
+
+Input - {1,2,3,4,5,6} and {9,8,7,6} 
+Output - { 1,9,2,8,3,7,4,6,5,6}
+*/
+
--- Codewars_C#_Cases/Program.cs
+++ Codewars_C#_Cases/Program.cs
@@ -2,6 +2,6 @@
 
 var result =  String_ends_with.Solution("abc", "bc");
 
-
+var result2 = CompoundArray.Solution(new int[] { 0, 1, 2 }, new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 });
 
 var i = 0;
